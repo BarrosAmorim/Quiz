f092693c5f76e950c531cc5a8004d3368ef04607
BarrosAmorim <barrosamorimd@gmail.com>
2021-12-24 20:44:35 -0300

    Component Resposta

--- components/Questao.tsx
+++ components/Questao.tsx
@@ -1,16 +1,40 @@
 import styles from '../styles/Questao.module.css'
 import QuestaoModel from "../models/questao";
 import Enunciado from './Enunciado';
+import Resposta from './Resposta';
+
+const letras = [
+    { valor: 'A', cor: '#f2c866'},
+    { valor: 'B', cor: '#f266ba'},
+    { valor: 'C', cor: '#85d4f2'},
+    { valor: 'D', cor: '#bce596'},
+]
 
 type QuestaoProps = {
     valor: QuestaoModel
+    respostaFornecida: (indice: number) => void
 }
 
 const Questao = (props: QuestaoProps) => {
     const questao = props.valor
+    function rederizarRespostas() {
+        return questao.respostas.map((resposta, i) => {
+            return (
+                <Resposta
+                    key={i}
+                    valor={resposta}
+                    indice={i}
+                    letra={letras[i].valor}
+                    corFundoLetra={letras[i].cor}
+                    respostaFornecida={props.respostaFornecida}
+                />
+            )
+        })
+    }
     return (
-        <div className={styles.container}>
-          <Enunciado texto={questao.enunciado} />
+        <div className={styles.questao}>
+            <Enunciado texto={questao.enunciado} />
+            {rederizarRespostas()}
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,27 @@ import styles from '../styles/Home.module.css'
 import Questao from '../components/Questao'
 import QuestaoModel from '../models/questao'
 import RespostaModel from '../models/resposta'
+import { useState } from 'react'
 
+
+const questaoTeste = new QuestaoModel(1, 'Melhor cor?', [
+  RespostaModel.errada('Verde'),
+  RespostaModel.certa('Azul'),
+  RespostaModel.errada('Rosa'),
+  RespostaModel.errada('Preta')
+])
 
 const Home: NextPage = () => {
-  const questaoTeste = new QuestaoModel(1, 'Melhor cor?', [
-    RespostaModel.errada('Verde'),
-    RespostaModel.errada('Azul'),
-    RespostaModel.errada('Rosa'),
-    RespostaModel.certa('Preta')
-  ])
+  const [questao, setQuestao] = useState(questaoTeste)
+
+  function respostaFornecida(indice: number) {
+    setQuestao(questao.responderCom(indice))
+    console.log(indice)
+  }
+
   return (
     <div className={styles.home}>
-      <Questao valor={questaoTeste} />
+      <Questao valor={questao}  respostaFornecida={respostaFornecida}/>
     </div>
   )
 }
